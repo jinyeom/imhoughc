2c47ec3f45fbac84b55e0321f097df0485c3197f
jinyeom <jinyeom95@gmail.com>
2018-03-04 17:39:45 -0600

    progress

--- houghcMain.m
+++ houghcMain.m
@@ -5,10 +5,16 @@ J = imread('images/planets.jpg');
 K = imread('images/emoji.png');
 
 % experiments...
-IC = detectCircles(I, 28);
+IC = detectCircles(I, 28); % detect pennys
 n = size(IC, 1);
 radii = zeros(n, 1);
 radii(:) = 28;
 imshow(I); hold on; viscircles(IC, radii);
 print('images/coins_result.png', '-dpng', '-r0'); close
 
+JC = detectCircles(J, 107); % detect Jupiter
+n = size(JC, 1);
+radii2 = zeros(n, 1);
+radii2(:) = 40;
+imshow(I); hold on; viscircles(JC, radii2);
+print('images/planets_result.png', '-dpng', '-r0'); close
